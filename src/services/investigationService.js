4b2dcd0854437549ce63df23dd9ea23ea22b081f
Munnerha <muneerha8@gmail.com>
2026-09-14 10:27:00 +0300

    Added index to investigation service

---
+++ src/services/investigationService.js
@@ -0,0 +1,20 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/investigations`;
+
+const formatError = (err) => {
+  const message = err instanceof Error ? err.message : String(err);
+  return new Error(message, { cause: err });
+};
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    const data = await res.json();
+    if (data.err) throw new Error(data.err);
+    return data.investigations;
+  } catch (err) {
+    console.log(err);
+    throw formatError(err);
+  }
+};
