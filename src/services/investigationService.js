ee4710725efd5e217620d1dafd92f70c11f6fe5c
Munnerha <muneerha8@gmail.com>
2026-09-14 11:01:04 +0300

    Added show and create to investigation service

--- src/services/investigationService.js
+++ src/services/investigationService.js
@@ -18,3 +18,36 @@ const index = async () => {
     throw formatError(err);
   }
 };
+
+const show = async (investigationId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${investigationId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    const data = await res.json();
+    if (data.err) throw new Error(data.err);
+    return data.investigation;
+  } catch (err) {
+    console.log(err);
+    throw formatError(err);
+  }
+};
+
+const create = async (formData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+            body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (data.err) throw new Error(data.err);
+        return data.investigation;
+    }   catch (err) {
+        console.log(err);
+        throw formatError(err);
+    }
+};
