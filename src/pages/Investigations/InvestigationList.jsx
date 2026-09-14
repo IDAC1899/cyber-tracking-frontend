4de579bf207d7c03cc80f7193a931bc1dbf5f3bc
Munnerha <muneerha8@gmail.com>
2026-09-14 17:49:03 +0300

    Added investigation list rendering

--- src/pages/Investigations/InvestigationList.jsx
+++ src/pages/Investigations/InvestigationList.jsx
@@ -19,3 +19,35 @@ const InvestigationList = () => {
     };
     fetchInvestigations();
   }, []);
+  return (
+    <main className='investigation-list-page'>
+      {/* page header: title + new investigation button */}
+      <div className='page-header'>
+        <h1>Investigations</h1>
+        <Link to='/investigations/new' className='btn btn-primary'>+ New Investigation</Link>
+      </div>
+
+      {/* error/status message */}
+      {message && <p className='error-message'>{message}</p>}
+
+      {/* investigation rows */}
+      <div className='investigation-list'>
+        {investigations.map((investigation) => (
+          <div key={investigation._id} className='investigation-row'>
+            <Link to={`/investigations/${investigation._id}`} className='investigation-row-link'>
+              <span className={`badge badge-priority-${investigation.priority.toLowerCase()}`}>
+                {investigation.priority}
+              </span>
+              <span className='investigation-title'>{investigation.title}</span>
+              <span className={`badge badge-status-${investigation.status.toLowerCase().replace(' ', '-')}`}>
+                {investigation.status}
+              </span>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default InvestigationList;
