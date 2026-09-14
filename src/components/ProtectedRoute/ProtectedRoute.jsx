ebe5bcc9011eb25a2d106940ed910cdac5208a7b
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-14 10:30:15 +0300

    added ProtectedRoute and ConfirmDelete to this branch as well

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+// src/components/ProtectedRoute/ProtectedRoute.jsx
+
+import { useContext } from 'react';
+import { Navigate } from 'react-router';
+
+import { UserContext } from '../../contexts/UserContext';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <Navigate to='/sign-in' />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
